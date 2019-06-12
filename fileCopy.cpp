f01c5b616dcfad4fa35bba2197630209851ec868
Gang-Ryung Uh <gangryunguh@gmail.com>
2019-06-12 13:27:48 -0500

    lab05 file exercises - file copy exercise

---
+++ fileCopy.cpp
@@ -0,0 +1,46 @@
+//
+// Created by Gang-Ryung Uh
+//
+// For "Line comment programming assignment"
+//
+// This program will prompt the user for the names
+// of input and output files.  It will then copy the
+// input file to the output file by byte-by-byte
+//
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    // Prompt the user for an input filename;
+    cout << "Enter an input file name: " << flush;
+    string infile;
+    cin >> infile;
+
+    // Check if the input file can be opened
+
+
+    // Prompt the user for an output filename
+    cout << "Enter an output file name: " << flush;
+    string outfile;
+    cin >> outfile;
+
+    // Check if the output file can be opened
+
+
+
+    // Copy characters from the input file to the output file
+
+
+
+
+
+    // close the input and output files
+
+
+
+    return 0;
+}
