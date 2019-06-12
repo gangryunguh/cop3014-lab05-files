0e2c0a23346d4b41b7f9ede2ad99b3dc5d3c499f
Gang-Ryung Uh <gangryunguh@gmail.com>
2019-06-12 13:56:58 -0500

    lab05 file exercises - file copy exercise solution

--- fileCopy.cpp
+++ fileCopy.cpp
@@ -7,6 +7,7 @@
 // of input and output files.  It will then copy the
 // input file to the output file by byte-by-byte
 //
+//
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -21,7 +22,15 @@ int main() {
     cin >> infile;
 
     // Check if the input file can be opened
-
+    ifstream fin;
+    fin.open(infile);
+    while (!fin) {
+        // input file does not exist, prompt the user again
+        cout << "Enter an input file name: " << flush;
+        string infile;
+        cin >> infile;
+        fin.open(infile);
+    }
 
     // Prompt the user for an output filename
     cout << "Enter an output file name: " << flush;
@@ -29,18 +38,22 @@ int main() {
     cin >> outfile;
 
     // Check if the output file can be opened
-
-
+    ofstream fout;
+    fout.open(outfile);
+    while (!fout) {
+        // output file cannot open, prompt the user again
+        cout << "Enter an output file name: " << flush;
+        string outfile;
+        cin >> outfile;
+        fout.open(outfile);
+    }
 
     // Copy characters from the input file to the output file
-
-
-
-
-
+    char c;
+    while ((c = fin.get()) != EOF)
+        fout << c;
     // close the input and output files
-
-
-
+    fout.close();
+    fin.close();
     return 0;
 }
